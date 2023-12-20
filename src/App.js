f55cd1bdeb95d4bf8854ecf38dc34aaf868e02d6
Suyash Jain <suyash.j@corp.cumulus>
2023-12-20 19:42:22 +0530

    Routes Creation Started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import {
+  BrowserRouter,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 
 //body and its components
 
@@ -14,5 +19,20 @@ const AppLayout = () => {
   );
 };
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "new/pew",
+    element: <h3>New1 just being got rendered!</h3>,
+  },
+  {
+    path: "new/pew",
+    element: <h3>New just being got rendered!</h3>,
+  },
+]);
+
 var root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={router} />);
