0e2eda36169655420ddfb0d56ac38c6cf175932d
Suyash Jain <suyash.j@corp.cumulus>
2024-02-28 22:38:53 +0530

    new menu design started

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -7,7 +7,6 @@ const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
   const { resId } = useParams();
   const [allMenus, setAllMenus] = useState([]);
-  const [currentMenu, setCurrentMenu] = useState([]);
 
   useEffect(() => {
     fetchMenu();
@@ -20,31 +19,24 @@ const RestaurantMenu = () => {
     console.log(json);
     setResInfo(json.data);
     setAllMenus(json.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
-    setCurrentMenu(
-      json.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-        ?.card?.itemCards
-    );
   };
 
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines } = resInfo?.cards[0]?.card?.card?.info;
+  const categories = allMenus.filter((menu) => {
+    let splitType = menu.card.card["@type"].split(".");
+    let element = splitType.pop();
+    return element === "ItemCategory";
+  });
+  console.log(categories);
 
   return (
-    <div className="menu">
+    <div className="text-center">
       <h1>{name}</h1>
       <h2>{cuisines.join(", ")}</h2>
       <h2>Menu</h2>
-      <ul>
-        ~||{allMenus[2].card.card.title}||~
-        {currentMenu.map((menu) => {
-          return (
-            <li key={menu.card.info.id}>
-              {menu.card.info.name} - {menu.card.info.price / 100}
-            </li>
-          );
-        })}
-      </ul>
+      <ul>~||{allMenus[2].card.card.title}||~</ul>
     </div>
   );
 };
